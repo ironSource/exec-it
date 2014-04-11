b91c0bef9d37793acf407d1cb935377fe9e3ae79
Yaniv Kessler <yanivk@gmail.com>
2014-04-11 15:47:27 +0300

    fixed incorrect exec shim, add --help / --usage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 
 var _ = require('lodash');
 var path = require('path');
@@ -12,6 +12,16 @@ var conf = rc('remote-exec', {
 	l: 'ec2-user',
 	i: process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] + '/.ssh/id_rsa'
 });
+
+if (conf.help || conf.usage) {
+	console.log(
+ 		'-c command to execute on remote machine\n' +
+ 		'-f read command from a file, works only if -c is not specified\n' +
+ 		'-p ssh port, default is 22\n' +
+ 		'-l username on remote machine, default is ec2-user\n' +
+ 		'-i private key, default is /a_home_dir/.ssh/id_rsa)\n')
+	return process.exit(0)
+}
 
 var command = (conf.c === undefined) ? fs.readFileSync(conf.f) : conf.c;
 
